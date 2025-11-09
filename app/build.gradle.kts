174fae6e88a6ca34d0c15d02e0aeb7fbe6d4611b
Leonel Parrales <leonelparrales22@gmail.com>
2025-11-09 15:39:52 -0500

    fix: update minSdk version from 24 to 21 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.vinilostsdc_frontend"
-        minSdk = 24
+        minSdk = 21
     targetSdk = 36
         versionCode = 1
         versionName = "1.0"
